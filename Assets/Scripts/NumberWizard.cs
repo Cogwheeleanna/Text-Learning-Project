9a5836356fadb0f6555525d98e21bcb14487765f
AlenaYatkina <yatkinalyona@gmail.com>
2020-11-16 23:20:02 +0300

    Encapsulating done
    small changes

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -11,13 +11,18 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-   
-        Debug.Log("Welcome to The Number Wizard!");
-        Debug.Log("Pick a number, please");
-        Debug.Log("The highest number is: "+ max);
-        Debug.Log("The lowest number is: "+ min);
-        Debug.Log("Tell me if your number is higher or lower than my guess");
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        Debug.Log("Hey! It's Number Wizard!");
+        Debug.Log("Pick a number, please! Don't tell me!");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
         Debug.Log("Push UP if your number is higher. Push DOWN if your number is lower. Push ENTER if my guess is correct.");
+        max = max + 1;
     }
 
     // Update is called once per frame
@@ -25,19 +30,23 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Ok, your number is higher");
             min = guess;
-            Debug.Log(guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Ok, your number is lower");
             max = guess;
-            Debug.Log(guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Ok, it's magic!");
+            Debug.Log("Great, it's magic!");
         }
+    }
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Ok, your number is higher OR lower than..." + guess);
+
     }
 }
