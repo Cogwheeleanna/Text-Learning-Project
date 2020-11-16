2d002c64165d5df4a1bd8ae61a97f7b1606eea04
AlenaYatkina <yatkinalyona@gmail.com>
2020-11-16 16:19:58 +0300

    Basic done

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
     // Start is called before the first frame update
     void Start()
     {
+   
         Debug.Log("Welcome to The Number Wizard!");
+        Debug.Log("Pick a number, please");
+        Debug.Log("The highest number is: "+ max);
+        Debug.Log("The lowest number is: "+ min);
+        Debug.Log("Tell me if your number is higher or lower than my guess");
+        Debug.Log("Push UP if your number is higher. Push DOWN if your number is lower. Push ENTER if my guess is correct.");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Ok, your number is higher");
+            min = guess;
+            Debug.Log(guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Ok, your number is lower");
+            max = guess;
+            Debug.Log(guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Ok, it's magic!");
+        }
     }
 }
